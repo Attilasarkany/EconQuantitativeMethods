4f96c709458d5d0465e91e9209f3fa4362b3edd4
Ying-Ting Lin <claire4621@gmail.com>
2016-05-03 21:01:54 -0500

    Added files via upload

---
+++ Project-4/valfun2.m
@@ -0,0 +1,21 @@
+function val=valfun2(k)
+%%Reference: http://www3.nd.edu/~esims1/val_fun_iter.pdf
+%Eric Sims, University of Notre Dame, Spring 2011
+%Continuous time value function iteration using linear splines
+%%
+global v0 beta delta alpha kmat k0 sigma P 
+
+kmin=max(sum(k>kmat),1); 
+kmax=kmin+1;
+
+% do the interpolation
+gg = v0(kmin,:) + (k-kmat(kmin))*(v0(kmax,:) - v0(kmin,:))/(kmat(kmax) - kmat(kmin));
+
+%exp(exp(logzgrid(2)))=2.7183
+c = 2.7183*k0^alpha - k + (1-delta)*k0; % consumption
+if c<=0
+val = -8888888888888888-800*abs(c);
+else
+val = (1/(1-sigma))*(c^(1-sigma)-1) + beta*(gg*P(2,:)');
+end
+val = -val; % make it negative since we're maximizing and code is to minimize.
